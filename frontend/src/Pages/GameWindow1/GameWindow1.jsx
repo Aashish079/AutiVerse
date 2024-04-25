4e667adf2c80326da88cd51d0b5fb91aae8c5d91
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-04-25 14:27:10 +0545

    Rest Backend

--- frontend/src/Pages/GameWindow1/GameWindow1.jsx
+++
@@ -1,9 +0,0 @@
-import FreehandCircleDrawer from '../../Components/FreehandCircleDrawer/FreehandCricleDrawer'
-
-const GameWindow1 = () => {
-  return (
-    <FreehandCircleDrawer />
-  )
-}
-
-export default GameWindow1
